c320fa8bd925a3752f86b4f8c0934a5dc1efbb00
BarryMolina <bazzaboy@gmail.com>
2020-03-06 07:50:54 -0600

    Election screen shots

---
+++ Molina_RectangleAppUnzipped/Molina_RectangleApp/Molina_RectangleApp/Dimensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+
+namespace Molina_RectangleApp
+{
+    public class Dimensions
+    {
+        private double height;
+        private double width;
+
+        public void InputHeight()
+        {
+            Write("\nPlease enter the rectangle's height in inches: ");
+            height = double.Parse(ReadLine());
+        }
+
+        public void InputWidth()
+        { 
+            Write("Please enter the rectangle's width in inches: ");
+            width = double.Parse(ReadLine());
+        }
+
+        public double CalculateArea()
+        {
+            return height * width;
+        }
+
+        public double CalculatePerimeter()
+        {
+            return 2 * height + 2 * width;
+        }
+    }
+}
---
+++ Molina_RectangleAppUnzipped/Molina_RectangleApp/Molina_RectangleApp/Molina_RectangleApp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static System.Console;
+
+namespace Molina_RectangleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dimensions dimensions = new Dimensions();
+
+            AssignementInfo();
+            Directions();
+
+            dimensions.InputHeight();
+            dimensions.InputWidth();
+
+            WriteLine("\nThe area of the rectangle is " + dimensions.CalculateArea());
+            WriteLine("The perimeter of the rectangle is {0:f1}", dimensions.CalculatePerimeter());
+
+            ReadKey();
+        }
+        static void Directions()
+        {
+            WriteLine("This program will calculate the area and the perimeter of a rectangle.");
+            WriteLine("All you need to do is enter the rectangle's height and width.\n");
+        }
+
+        static void AssignementInfo()
+        {
+            WriteLine("******************************************************\n");
+            WriteLine("{0,-15}Barry Molina", "Name:");
+            WriteLine("{0,-15}ITDEV-115-200", "Course:");
+            WriteLine("{0,-15}J. Christie", "Instructor:");
+            WriteLine("{0,-15}Rectangle Assignment", "Assignment:");
+            WriteLine("{0,-15}1/28/2020", "Date:");
+            WriteLine("\n******************************************************\n\n");
+        }
+    }
+}
